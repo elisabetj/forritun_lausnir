9828c41a1fad23d2f0c15ff368c616f99a5b161b
PeturOA <peturolafura@gmail.com>
2023-09-10 03:48:03 +0000

    Add blank lines.

--- assignments/02_decisions_and_booleans/a02p03budget/complete_autograder_setup/submissions/accepted/solution_code.py
+++ assignments/02_decisions_and_booleans/a02p03budget/complete_autograder_setup/submissions/accepted/solution_code.py
@@ -1,7 +1,9 @@
 budget = int(input())
+
 project1 = int(input())
 project2 = int(input())
 project3 = int(input())
+
 if project1 + project2 + project3 <= budget:
     print("Budget is sufficient.")
 else:
