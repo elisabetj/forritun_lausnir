bbcb54134185c829b58ea8c768bdbe643f4a83bd
PeturOA <peturolafura@gmail.com>
2023-09-10 03:19:41 +0000

    Remove old comment. This alternative is in a separate file.

--- assignments/02_decisions_and_booleans/a02p02findmaximum/complete_autograder_setup/submissions/accepted/solution_code.py
+++ assignments/02_decisions_and_booleans/a02p02findmaximum/complete_autograder_setup/submissions/accepted/solution_code.py
@@ -9,11 +9,3 @@ if num3 > max_int:
     max_int = num3
 
 print(max_int)
-
-# Alternatively:
-# if num1 > num2 and num1 > num3:
-#     max_int = num1
-# elif num2 > num3:
-#     max_int = num2
-# else:
-#     max_int = num3
