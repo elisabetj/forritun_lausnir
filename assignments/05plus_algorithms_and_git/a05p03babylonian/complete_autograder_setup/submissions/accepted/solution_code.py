b47f603bfa6fd77cc5ade84e3d73cea529f0826d
PeturOA <peturolafura@gmail.com>
2023-09-17 09:32:43 +0000

    Add assertions.

--- assignments/05plus_algorithms_and_git/a05p03babylonian/complete_autograder_setup/submissions/accepted/solution_code.py
+++ assignments/05plus_algorithms_and_git/a05p03babylonian/complete_autograder_setup/submissions/accepted/solution_code.py
@@ -6,11 +6,17 @@ tolerance = float(input())
 count = 0  # Count the number of guesses.
 previous = 0  # Track the previous calculated value.
 
+assert abs(previous - guess) > tolerance
+# The input assurances in the problem description guarantee this,
+# but it is nevertheless a good idea to document this assumption to make it explicit.
+
 # Until we are within tolerance, keep inching our way towards the square root.
 while abs(previous - guess) > tolerance:
     previous = guess
+    assert guess != 0  # Before.
     quotient = number / guess
     guess = (quotient + guess) / 2
+    assert guess != 0  # And after.
     count += 1
 
 print(guess)
