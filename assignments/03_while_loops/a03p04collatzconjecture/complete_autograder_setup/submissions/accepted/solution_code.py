e613035e9303ecc5be9e134c65def32b636b12a2
PeturOA <peturolafura@gmail.com>
2023-09-17 07:01:14 +0000

    Fix punctuation.

--- assignments/03_while_loops/a03p04collatzconjecture/complete_autograder_setup/submissions/accepted/solution_code.py
+++ assignments/03_while_loops/a03p04collatzconjecture/complete_autograder_setup/submissions/accepted/solution_code.py
@@ -2,8 +2,8 @@ x = int(input())
 print(x)
 
 while x > 1:
-    if x % 2 == 0:  # then x is even
+    if x % 2 == 0:  # Then x is even.
         x = x // 2
-    else:  # then x is odd
+    else:  # Then x is odd.
         x = 3 * x + 1
     print(x)
